7be266b06733b2b6901df800d218f45e3d321088
Anurag Chaurasia <anuragchaurasia1020@gmail.com>
2026-07-02 17:11:45 +0530

    update backend url

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -5,7 +5,7 @@ function Hero() {
     return ( 
         <div className='container p-5 mb-5'>
             <div className='row text-center'>
-                <img src="media/images/HomeHero.png" alt="HeroImage" className='mb-5'/>
+                <img src="media/images/homeHero.png" alt="HeroImage" className='mb-5'/>
                 <h1 className='mt-5'>Invest in everything</h1>
                 <p>Online Plateforms to invest in stocks derivatives , mutual funds and more </p>
              <Link to="/Signup"> <button  className='p-2 btn btn-primary  fs-5 mb-5' style={{width:"17%", margin:"0 auto"}}> Signup Now</button> </Link>   
