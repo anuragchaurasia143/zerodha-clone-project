c493e6fa25a38ee8b1a3c4a59a3935b1921743ef
Anurag Chaurasia <anuragchaurasia1020@gmail.com>
2026-07-03 09:41:07 +0530

    update backend url

--- frontend/src/landing_page/signup/Signup.js
+++ frontend/src/landing_page/signup/Signup.js
@@ -20,7 +20,7 @@ function Signup() {
     e.preventDefault();
 
     try {
-      const res = await axios.post("https://zerodha-backend-3fi6.onrender.com//signup", user);
+      const res = await axios.post("https://zerodha-backend-3fi6.onrender.com/signup", user);
 
       alert(res.data.message);
     } catch (err) {
