945c3093a438bf5114e063f26b630241cd6378cd
Anurag Chaurasia <anuragchaurasia1020@gmail.com>
2026-07-03 10:25:15 +0530

    update backend url

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -11,7 +11,7 @@ const Holdings = () => {
   const [allHoldings, setAllHoldings] = useState([]);
 
   useEffect(() => {
-    axios.get("https://zerodha-backend-3fi6.onrender.com//allHoldings").then((res) => {
+    axios.get("https://zerodha-backend-3fi6.onrender.com/allHoldings").then((res) => {
       // console.log(res.data);
       setAllHoldings(res.data);
     });
